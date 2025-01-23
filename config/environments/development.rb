c9627880b979627742d05b13d94d22c3134acd3e
Morishita <makaserori3022>
2025-01-23 15:52:46 +0000

    susunda

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+      config.hosts << "b7b808b263d3425e9a04ce7471555a55.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
